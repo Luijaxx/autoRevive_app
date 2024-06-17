e19d4e37344b353a9eb46d2ec30167685f7bc88c
Luijaxx <luija2000@gmail.com>
2024-06-16 18:42:20 -0600

    Angular funcionando version 17, version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ShareModule } from './share/share.module';
+import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  // TO DO modules [all in bd]
   imports: [
+    //module order 
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule, 
+    CoreModule,
+    ShareModule,
+    HomeModule,
+    UserModule,
+    // router manager
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  animations: [
+    trigger('dropdownAnimation', [
+      state('open', style({
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+      state('closed', style({
+        opacity: 0,
+        transform: 'translateY(-1rem)'
+      })),
+      transition('closed => open', [
+        animate('5000ms ease-out')
+      ]),
+      transition('open => closed', [
+        animate('5000ms ease-in')
+      ]),
+    ])
+  ]
+})
+export class HeaderComponent {
+  isOpen = false;
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeDropdown() {
+    this.isOpen = false;
+  }
+}
